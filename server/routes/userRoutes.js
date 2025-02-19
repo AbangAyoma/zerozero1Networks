d537c812af908d341360512c3dc4eabccfdf474e
AbangAyoma <abangayoma@gmail.com>
2025-02-19 12:26:46 +0100

    🚀 Updated userRoutes.js with authentication routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getUserProfile } from "../controllers/userController.js";
+import { registerUser, loginUser, getUserProfile } from "../controllers/userController.js";
 
 const router = express.Router();
 
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/profile", getUserProfile);
 
 export default router;
